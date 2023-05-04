73dd960b6db81aa6731635aacf76aea0498fc872
Simon <simonleary@umass.edu>
2023-05-04 13:49:49 -0400

    add comments, don't put stage in /scratch

--- unity/installers/install-package.sh
+++ unity/installers/install-package.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 PREFIX="/modules/spack-0.19.1/unity/installers"
 ARCHITECTURES="x86_64,ppc64le,aarch64"
@@ -16,12 +15,14 @@ read -r -d '' help <<- HELP
 
 	usage:
 	install-package.sh [spack package spec]
-	install-package.sh [-a arch] [-c cpus] [-p partition] [-t time] [-d|f|h|y] [spack package spec]
+	install-package.sh [-a arch] [-c cpus] [-p partition] [-t time] [-d|f|h|y] "[spack package spec]"
 
 	examples:
-		install-package.sh -yf -a ppc64le apptainer@1.1.5+suid
-		EXTRA_SPACK_ARGS="--use-buildcache never" install-package.sh apptainer@1.1.5+suid
-		EXTRA_SBATCH_ARGS="-G 1" install-package.sh apptainer@1.1.5+suid
+		install-package.sh zlib
+		install-package.sh "netlib-scalapack ^openblas ^openmpi"
+		install-package.sh -yf -a x86_64,ppc64le zlib
+		EXTRA_SPACK_ARGS="--use-buildcache never" install-package.sh zlib
+		EXTRA_SBATCH_ARGS="-G 1" install-package.sh zlib
 
 	spack package spec -> https://spack.readthedocs.io/en/latest/basic_usage.html#sec-specs
 	spack install args -> https://spack.readthedocs.io/en/latest/command_index.html#spack-install
@@ -41,7 +42,10 @@ read -r -d '' help <<- HELP
 
 	environment variables:
 		EXTRA_SPACK_ARGS: \`spack install \$EXTRA_SPACK_ARGS [spack package spec]\`
-		EXTRA_SBATCH_ARGS: \`sbatch \$EXTRA_SBATCH_ARGS slurm-install-batch.sh\`
+		EXTRA_SBATCH_ARGS: \`sbatch \$EXTRA_SBATCH_ARGS batch-script.sh\`
+
+	assumptions:
+		* your -a architecture used for spack target=... is also valid as a slurm feature/constraint
 
 HELP
 
@@ -111,7 +115,7 @@ if (( $# > 0 )); then
 	exit 1
 fi
 
-BATCH_SCRIPT_PATH=$(mktemp)
+BATCH_SCRIPT_PATH=$(mktemp --suffix=".sh")
 # just for peace of mind, shouldn't ever happen
 if [ ! -e "$BATCH_SCRIPT_PATH" ]; then
 	echo "mktemp was supposed to create file \"$BATCH_SCRIPT_PATH\" but it doesn't exist!"
@@ -119,42 +123,31 @@ if [ ! -e "$BATCH_SCRIPT_PATH" ]; then
 fi
 cat <<- BATCH_SCRIPT > $BATCH_SCRIPT_PATH
 	#!/bin/bash
+	set -e
+	if [ ! -z "\$TMPDIR_NAME" ]; then
+		export TMPDIR=$PREFIX/install-tmp/\$TMPDIR_NAME/
+		echo "you can find my source files and build logs in \$TMPDIR"
+		mkdir \$TMPDIR
+	fi
 	echo "jobid \$SLURM_JOB_ID on host \$(hostname) by user \$(whoami) on \$(date)"
-	source $PREFIX/../share/spack/setup-env.sh
+	source $PREFIX/../../share/spack/setup-env.sh
 	echo "spack $DEBUG install \$SPACK_INSTALL_ARGS"
 	spack $DEBUG install \$SPACK_INSTALL_ARGS
-	if [ ! \$? -eq 0 ]; then
-		# copy spack-build-out and spack-build-env in case we need to make a github issue
-		stage_dir=/scratch/\$(hostname)/\$SLURM_JOB_ID/\$(whoami)/spack-stage/
-		if [ -d \$stage_dir ]; then
-			ARGS_LIST=(\$SPACK_INSTALL_ARGS)
-			# 2nd to last argument is the package name, last argument is the target architecture
-			stage_copy_dir=$PREFIX/logs/failed-builds/\$(date +%s)-\${ARGS_LIST[-2]}-\${ARGS_LIST[-1]}
-			mkdir -p \$stage_copy_dir
-			cp \$stage_dir \$stage_copy_dir/
-			cp \$stage_dir \$stage_copy_dir/
-			echo "spack stage copied to \$stage_copy_dir/"
-		else
-			echo "spack stage directory \$stage_dir doesn't exist!"
-		fi
-	fi
 BATCH_SCRIPT
 
+# build an array of job commands and an array of log files
 IFS="," # comma separated list
 for arch in $ARCHITECTURES; do
 	if [ -z $(echo $arch | xargs) ]; then continue; fi # ignore blank
-	SPACK_INSTALL_ARGS="\
-$FRESH_OR_REUSE -y --keep-stage --fail-fast \
-$EXTRA_SPACK_ARGS $PACKAGE_SPEC target=$arch"
+	SPACK_INSTALL_ARGS="$FRESH_OR_REUSE -y --keep-stage --fail-fast --source $EXTRA_SPACK_ARGS $PACKAGE_SPEC target=$arch"
+	# format the spec so that it's suitable for the name of a log file
+	PACKAGE_SPEC_CLEANED=${PACKAGE_SPEC// /-} # replace [:space:] with -
+	PACKAGE_SPEC_CLEANED=${PACKAGE_SPEC_CLEANED/\//-} # replace / with -
 	# include time so that `ls` sorts chronologically
-	LOG_FILE="$PREFIX/logs/$(date +%s)-${PACKAGE_SPEC}-${arch}.out"
+	LOG_FILE="$PREFIX/logs/$(date +%s)-${PACKAGE_SPEC_CLEANED}-${arch}.out"
 	touch $LOG_FILE # make sure we have permissions to the log file
 	log_files+=("$LOG_FILE")
-	this_job="\
-sbatch --job-name=\"$PACKAGE_SPEC-$arch\" --output=\"$LOG_FILE\" --partition=\"$PARTITION\" \
---cpus-per-task=\"$CPUS_PER_TASK\" --mem=\"$MEMORY\" --time=\"$TIME\" -N 1 \
---export=\"SPACK_INSTALL_ARGS=$SPACK_INSTALL_ARGS\" --constraint=$arch \
-$EXTRA_SBATCH_ARGS $BATCH_SCRIPT_PATH"
+	this_job="sbatch --job-name=\"$PACKAGE_SPEC-$arch\" --output=\"$LOG_FILE\" --partition=\"$PARTITION\" --cpus-per-task=\"$CPUS_PER_TASK\" --mem=\"$MEMORY\" --time=\"$TIME\" -N 1 --export=\"SPACK_INSTALL_ARGS=$SPACK_INSTALL_ARGS,TMPDIR_NAME=$PACKAGE_SPEC_CLEANED\" --constraint=$arch $EXTRA_SBATCH_ARGS $BATCH_SCRIPT_PATH"
 	echo "$this_job"
 	echo
 	JOBS+=("$this_job")
@@ -164,6 +157,7 @@ unset IFS
 if [[ $DO_SKIP_PROMPT == "true" ]] || prompt_yes_or_no "do you want to submit these jobs?"; then
 	echo
 	IFS=$'\n'
+	# execute each job command and print the logfile that it should output to
 	for i in ${!JOBS[@]}; do
 		job=${JOBS[$i]}
 		logfile=${log_files[$i]}
