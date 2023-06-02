b10d3169896a3b6429e01b1f8b548d0f15c2cdba
simonleary-umass-edu <simonleary@umass.edu>
2023-06-02 18:05:42 -0400

    update the spider cache script

--- unity/installers/update-spider-cache.sh
+++ unity/installers/update-spider-cache.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
-# assuming that all module directories are accessible by loading modules out of /modules/modulefiles
-# spack directories: module load microarch, URI directories: module load uri, ...
+prepend_modulepath() {
+    if [ ! -d "$1" ]; then
+        echo "\"$1\" does not exist!"
+        return 1
+    fi
+    # if $1 is already present in modulepath
+    if [[ $MODULEPATH =~ (^|:)"$1"($|:) ]] ; then
+        return 1
+    fi
+    if [ -z $MODULEPATH ]; then
+        export MODULEPATH="$1"
+    else
+        export MODULEPATH="$1:$MODULEPATH"
+    fi
+}
+
 MODULEPATH="/modules/modulefiles"
+# the microarch modules no longer cover production spack
+# add each architecture's Core directory to modulepath
+for arch in $(ls ../../share/spack/lmod/ | grep ^linux-ubuntu); do
+    prepend_modulepath($(dirname $(dirname $PWD))/share/spack/lmod/$arch/Core)
+
 # -D debug
 source /usr/share/lmod/lmod/libexec/update_lmod_system_cache_files -D $MODULEPATH
