e1f2c55703307dabcceb1d91b8fe909416b8e385
simonleary-umass-edu <simonleary@umass.edu>
2023-06-02 20:12:30 -0400

    stop trying to build cache for all architectures at the same time

--- unity/installers/update-spider-cache.sh
+++ unity/installers/update-spider-cache.sh
@@ -2,28 +2,5 @@
 #SBATCH -c 1
 #SBATCH -p cpu-preempt
 
-prepend_modulepath() {
-    if [ ! -d "$1" ]; then
-        echo "\"$1\" does not exist!"
-        return 1
-    fi
-    # if $1 is already present in modulepath
-    if [[ $MODULEPATH =~ (^|:)"$1"($|:) ]] ; then
-        return 1
-    fi
-    if [ -z $MODULEPATH ]; then
-        export MODULEPATH="$1"
-    else
-        export MODULEPATH="$1:$MODULEPATH"
-    fi
-}
-
-MODULEPATH="/modules/modulefiles"
-# the microarch modules no longer cover production spack
-# add each architecture's Core directory to modulepath
-for arch in $(ls ../../share/spack/lmod/ | grep ^linux-ubuntu); do
-    prepend_modulepath "$(dirname $(dirname $PWD))/share/spack/lmod/$arch/Core"
-done
-
 # -D debug
 source /usr/share/lmod/lmod/libexec/update_lmod_system_cache_files -D $MODULEPATH
