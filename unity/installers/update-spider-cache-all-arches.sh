dd12a621c438721500178751034b9ac4ff41ae8d
Simon <simonleary@umass.edu>
2023-06-08 16:05:56 -0400

    new update-spider-cache.sh

--- unity/installers/update-spider-cache-all-arches.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-TARGETS="x86_64 aarch64 ppc64le"
-for target in $TARGETS; do
-    sbatch --constraint=$target update-spider-cache.sh
-done
-
--- unity/installers/update-spider-cache.sh
+++ unity/installers/update-spider-cache.sh
@@ -3,5 +3,19 @@
 #SBATCH -p cpu-preempt
 #SBATCH --export=none
 
-# -D debug
-source /usr/share/lmod/lmod/libexec/update_lmod_system_cache_files -D $MODULEPATH
+set -e
+
+arches="x86_64,ppc64le,aarch64"
+IFS=','
+for arch in $arches; do
+    cmd="\
+/usr/share/lmod/lmod/libexec/update_lmod_system_cache_files \
+-D \
+-d \"/modules/lmod/spider-cache/$arch\" \
+-t \"/modules/lmod/spider-cache/$arch/system.txt\" \
+\"/modules/modulefiles:/modules/spack_modulefiles/linux-ubuntu20.04-$arch/Core\"\
+"
+    echo "$cmd"
+    eval $cmd
+done
+unset IFS
