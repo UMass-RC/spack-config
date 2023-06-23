a7f5498ad4239c30065c80344c1db22a5dc0ded1
Simon <simonleary@umass.edu>
2023-06-23 15:15:32 -0400

    update `update-spider-cache.sh`

--- unity/installers/update-spider-cache.sh
+++ unity/installers/update-spider-cache.sh
@@ -8,12 +8,13 @@ set -e
 arches="x86_64,ppc64le,aarch64"
 IFS=','
 for arch in $arches; do
+    os_arch=linux-ubuntu20.04-$arch
     cmd="\
 /usr/share/lmod/lmod/libexec/update_lmod_system_cache_files \
 -D \
 -d \"/modules/lmod/spider-cache/$arch\" \
 -t \"/modules/lmod/spider-cache/$arch/system.txt\" \
-\"/modules/modulefiles:/modules/spack_modulefiles/linux-ubuntu20.04-$arch/Core\"\
+\"/modules/modulefiles/noarch:/modules/modulefiles/$arch:/modules/spack_modulefiles/$os_arch/Core\"\
 "
     echo "$cmd"
     eval $cmd
