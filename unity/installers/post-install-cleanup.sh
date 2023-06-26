069ff1188d8f9a57360dfb96ebd0926f53f282e9
Simon <simonleary@umass.edu>
2023-06-26 10:50:16 -0400

    add post-install-cleanup.sh

---
+++ unity/installers/post-install-cleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -c 4
+#SBATCH --mem=16G
+#SBATCH -p cpu-preempt
+#SBATCH -t 8:00:00
+
+SPACK_PREFIX=/modules/spack/0.20.0/
+
+if [ -z "$SLURM_JOB_ID" ]; then
+    echo "this script takes a long time, you probably want to \`sbatch\` me!"
+    echo "sleeping for 3 seconds in case you want to ctrl+c..."
+    sleep 3
+fi
+set -x
+source $SPACK_PREFIX/share/spack/setup-env.sh
+cd $SPACK_PREFIX/unity/installers
+spack gc -y # uninstall packages that were needed only at build time
+./hide-implicit-modules.sh # hide implicit modules (installed as dependencies) from Lmod
+./update-spider-cache.sh # update Lmod spider cache
+
